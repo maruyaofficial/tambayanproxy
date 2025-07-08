49c86bfe48c3a2a9adeb4125bd3fb958afa81dae
maruyaofficial <jasminemontirola@gmail.com>
2025-07-09 02:59:41 +0800

    Update channels.js

--- channels.js
+++ channels.js
@@ -25,8 +25,7 @@ const defaultChannelList = [
   },
   {
     name: "Cinema One",
-    manifest: 'https://tambayan-proxy.vercel.app/api/proxy?url=https://cdn-ue1-prod.tsv2.amagi.tv/linear/amg01006-abs-cbn-cinemaone-dash-abscbnono/index.mpd
-',
+    manifest: 'https://proxy.tambayan-chat.site/abs-cbn-cinemaone-dash-abscbnono.mpd',
     drm: null,
     format: "dash"
   },
